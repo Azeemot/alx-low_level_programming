5b57b27f4cf1658a34111bec1eb9ed84a4c184b8
Azeemot <bolanleazeemah@gmail.com>
2022-09-08 18:13:18 -0700

    completed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,16 @@
-#include<stdio.h>
+#include <stdio.h>
+
+
 
 /**
  *
- * *main - Entry point
+ *  * main - Entry point
  *
- * *
+ *   * Description: prints "\"Programming is like building a multilingual puzzle"
  *
- * *Return: Always 0 (Success)
+ *    * Return: 0
  *
- * */
+ *     */
 
 
 
