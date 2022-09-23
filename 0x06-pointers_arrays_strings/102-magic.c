ca33b3be7a0c7a83f71881496c2a16d20191920e
Azeemot <bolanleazeemah@gmail.com>
2022-09-22 17:11:23 -0700

    completed

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,21 +1,17 @@
 #include <stdio.h>
-#include <main.h>
-/**
- * main - consists of all.
- * Description: prints magic.
- * Return: 0
- */
 
 int main(void)
 {
 int n;
 int a[5];
 int *p;
+
 a[2] = 1024;
 p = &n;
   /*
    * write your line of code here...
    * Remember:
+   * main - magic
    * - you are not allowed to use a
    * - you are not allowed to modify p
    * - only one statement
