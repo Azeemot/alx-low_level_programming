9504d7b7cd9cae647c3aa4786ac7f4395ae02a4e
Azeemot <bolanleazeemah@gmail.com>
2022-09-30 17:36:15 -0700

    Create the static library libmy.a

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,19 +1,17 @@
 #include "main.h"
 
 /**
- * _isupper - checks whether a character is uppercase.
- *@c: the integer to be supplied by user.
- * Return: 0 or 1.
- */
+*_isupper - checks if a  character is uppercase or not
+*@c: character to be tested
+*Return: 1 whether it is, 0 otherwise
+*/
 
 int _isupper(int c)
 {
-if (c >= 65 && c <= 90)
-{
-return (1);
-}
-else
-{
-return (0);
-}
+	if ((c >= 'A') && (c <= 'Z'))
+	{
+		return (1);
+	}
+
+	return (0);
 }
